181dd5356a8607f2e6be4673975ba8ce459ee856
Pavel Izmailov <izmailovpavel@gmail.com>
2021-06-22 12:11:27 -0400

    First commit

---
+++ bnn_hmc/utils/cmd_args_utils.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility functions for command line arguments."""
+
+import os
+import sys
+import tensorflow.compat.v2 as tf
+
+
+def add_common_flags(parser):
+  parser.add_argument("--tpu_ip", type=str, default=None,
+                      help="Cloud TPU internal ip "
+                           "(see `gcloud compute tpus list`)")
+  parser.add_argument("--seed", type=int, default=0, help="Random seed")
+  parser.add_argument("--temperature", type=float, default=1.,
+                      help="Temperature of the posterior")
+  parser.add_argument("--init_checkpoint", type=str, default=None,
+                      help="Checkpoint to use for initialization of the chain")
+  parser.add_argument("--tabulate_freq", type=int, default=40,
+                      help="Frequency of tabulate table header prints")
+  parser.add_argument("--dir", type=str, default=None, required=True,
+                      help="Directory for checkpoints and tensorboard logs")
+  parser.add_argument("--dataset_name", type=str, default="cifar10",
+                      help="Name of the dataset")
+  parser.add_argument("--subset_train_to", type=int, default=None,
+                      help="Size of the subset of train data to use; "
+                           "full dataset is used by default")
+  parser.add_argument("--model_name", type=str, default="lenet",
+                      help="Name of the dataset")
+  parser.add_argument("--use_float64", dest="use_float64", action="store_true",
+                      help="Use float64 precision (does not work on TPUs)")
+
+  # Prior
+  parser.add_argument("--prior_family", type=str, default="Gaussian",
+                      choices=["Gaussian", "ExpFNormP", "Laplace", "StudentT",
+                               "DiagGaussian", "LenetConvCorrelated",
+                               "LeNetSumFilter", "LeNetPCA", "MLPPCA"],
+                      help="Prior distribution family")
+  parser.add_argument("--weight_decay", type=float, default=15.,
+                      help="Weight decay, equivalent to setting prior std")
+  parser.add_argument("--studentt_degrees_of_freedom", type=float, default=1.,
+                      help="Degrees of freedom in StudentT family")
+  parser.add_argument("--expfnormp_power", type=float, default=2.,
+                      help="Power of the F-norm in ExpFNormp family")
+  parser.add_argument("--diaggaussian_cov_ckpt", type=str, default=None,
+                      help="Path to the checkpoint containing the diagonal of"
+                           "the covariance matrix for DiagGaussian family")
+  parser.add_argument("--diaggaussian_cov_scale", type=float, default=1.,
+                      help="Scaling factor for the covariance matrix for "
+                           "DiagGaussian family")
+  # parser.add_argument("--lenetconvcorrelated_cov_ckpt", type=str, default=None,
+  #                     help="Path to the .npy checkpoint containing the inverse "
+  #                          "of the covariance matrix of the first LeNet5 Layer "
+  #                          "for the LenetConvCorrelated family")
+  parser.add_argument("--lenetconvcorrelated_invcov_scale", type=float,
+                      default=1.,
+                      help="Inv cov scale for the LenetConvCorrelated family")
+  parser.add_argument("--lenetsumfilter_weight_decay", type=float, default=15.,
+                      help="Weight decay for the Laplace prior over the sum of"
+                           "the filter weights in the first layer of LeNet5"
+                           "for the LeNetSumFilter prior family")
+  parser.add_argument("--lenetpca_invcov_ckpt", type=str, default=None,
+                      help="Path to the .npy checkpoint containing the inverse "
+                           "of the covariance matrix of the first LeNet5 Layer "
+                           "for the LeNetPCA family")
+  parser.add_argument("--lenetpca_pca_wd", type=float, default=None,
+                      help="Weight decay for the PCA term"
+                           "for the LeNetPCA family")
+
+def add_sgd_flags(parser):
+
+  parser.add_argument("--init_step_size", type=float, default=1.e-6,
+                      help="Initial SGD step size")
+  parser.add_argument("--num_epochs", type=int, default=300,
+                      help="Total number of SGD epochs iterations")
+  parser.add_argument("--batch_size", type=int, default=80, help="Batch size")
+  parser.add_argument("--eval_freq", type=int, default=10,
+                      help="Frequency of evaluation (epochs)")
+  parser.add_argument("--save_freq", type=int, default=50,
+                      help="Frequency of checkpointing (epochs)")
+  parser.add_argument("--momentum_decay", type=float, default=0.9,
+                      help="Momentum decay parameter for SGD")
+
+
+def save_cmd(dirname, tf_writer):
+  command = " ".join(sys.argv)
+  with open(os.path.join(dirname, "command.sh"), "w") as f:
+    f.write(command)
+    f.write("\n")
+  if tf_writer is not None:
+    with tf_writer.as_default():
+      tf.summary.text("command", command, step=0,
+                      description="Command line arguments")
---
+++ bnn_hmc/utils/losses.py
@@ -0,0 +1,254 @@
+# coding=utf-8
+# Copyright 2021 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Likelihood and prior functions."""
+import math
+
+import jax
+import jax.numpy as jnp
+
+from bnn_hmc.utils import tree_utils
+
+
+def make_xent_log_likelihood(temperature):
+  def xent_log_likelihood(net_apply, params, net_state, batch, is_training):
+    """Computes the negative log-likelihood."""
+    _, y = batch
+    logits, net_state = net_apply(params, net_state, None, batch, is_training)
+    num_classes = logits.shape[-1]
+    labels = jax.nn.one_hot(y, num_classes)
+    softmax_xent = jnp.sum(labels * jax.nn.log_softmax(logits)) / temperature
+
+    return softmax_xent, net_state
+  return xent_log_likelihood
+
+
+def preprocess_network_outputs_gaussian(predictions):
+  """Apply softplus to std output if available.
+
+  Returns predictive mean and standard deviation.
+  """
+  predictions_mean, predictions_std = jnp.split(predictions, [1], axis=-1)
+  predictions_std = jax.nn.softplus(predictions_std)
+  return jnp.concatenate([predictions_mean, predictions_std], axis=-1)
+
+
+def make_gaussian_likelihood(temperature):
+  def gaussian_log_likelihood(net_apply, params, net_state, batch, is_training):
+    """Computes the negative log-likelihood.
+
+    The outputs of the network should be two-dimensional.
+    The first output is treated as predictive mean. The second output is treated
+    as inverse-softplus of the predictive standard deviation.
+    """
+    _, y = batch
+    predictions, net_state = net_apply(
+        params, net_state, None, batch, is_training)
+
+    predictions = preprocess_network_outputs_gaussian(predictions)
+    predictions_mean, predictions_std = jnp.split(predictions, [1], axis=-1)
+    tempered_std = predictions_std * jnp.sqrt(temperature)
+      
+    se = (predictions_mean - y)**2
+    log_likelihood = (-0.5 * se / tempered_std**2
+                      - 0.5 * jnp.log(tempered_std**2 * 2 * math.pi))
+    log_likelihood = jnp.sum(log_likelihood)
+    
+    return log_likelihood, net_state
+  return gaussian_log_likelihood
+
+
+def make_gaussian_log_prior(weight_decay, temperature):
+  """Returns the Gaussian log-density and delta given weight decay."""
+
+  def log_prior(params):
+    """Computes the Gaussian prior log-density."""
+    # ToDo izmailovpavel: make temperature treatment the same as in gaussian
+    # likelihood function.
+    n_params = sum([p.size for p in jax.tree_leaves(params)])
+    log_prob = -(0.5 * tree_utils.tree_dot(params, params) * weight_decay +
+                 0.5 * n_params * jnp.log(weight_decay / (2 * math.pi)))
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    """Computes the delta in  Gaussian prior log-density."""
+    diff = sum([
+        jnp.sum(p1**2 - p2**2)
+        for p1, p2 in zip(jax.tree_leaves(params1), jax.tree_leaves(params2))
+    ])
+    return -0.5 * weight_decay * diff / temperature
+
+  return log_prior, log_prior_diff
+
+
+def make_laplace_log_prior(weight_decay, temperature):
+  """Returns the Gaussian log-density and delta given weight decay."""
+
+  b = jnp.sqrt(1 / 2 / weight_decay) # 2 * b**2 == 1/ weight_decay
+
+  def log_prior(params):
+    """Computes the Gaussian prior log-density."""
+    n_params = sum([p.size for p in jax.tree_leaves(params)])
+    l1_norm = sum([jnp.abs(p).sum() for p in jax.tree_leaves(params)])
+    log_prob = -l1_norm / b - jnp.log(2 * b)
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    """Computes the delta in  Gaussian prior log-density."""
+    diff = sum([
+        (jnp.abs(p1) - jnp.abs(p2)).sum()
+        for p1, p2 in zip(jax.tree_leaves(params1), jax.tree_leaves(params2))
+    ])
+    return  -diff / b / temperature
+
+  return log_prior, log_prior_diff
+
+
+def make_student_t_log_prior(weight_decay, degrees_of_freedom, temperature):
+  def log_prior(params):
+    log_prob = sum([jnp.log(1 + p**2 * weight_decay / degrees_of_freedom).sum()
+                    for p in jax.tree_leaves(params)])
+    log_prob *= -(degrees_of_freedom + 1) / 2
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    return log_prior(params1) - log_prior(params2)
+
+  return log_prior, log_prior_diff
+
+
+def make_exp_fnorm_p_log_prior(weight_decay, power, temperature):
+  """Returns the exp(- F-norm^p) prior log-density and delta given weight decay.
+
+  Gaussian prior corresponds to power p=2.
+  """
+
+  def log_prior(params):
+    """Computes the Gaussian prior log-density up to a constant."""
+    n_params = sum([p.size for p in jax.tree_leaves(params)])
+    fnorm_p = (tree_utils.tree_dot(params, params))**(power / 2)
+    log_prob = -0.5 * fnorm_p * weight_decay
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    """Computes the delta in  Gaussian prior log-density."""
+    return log_prior(params1) - log_prior(params2)
+
+  return log_prior, log_prior_diff
+
+
+def make_diag_gaussian_log_prior(cov_diag_tree, temperature):
+  """Returns the Gaussian log-density and delta given weight decay."""
+  n_params = sum([p.size for p in jax.tree_leaves(cov_diag_tree)])
+  cov_logdet = sum(jax.tree_leaves(jax.tree_map(
+      lambda s: jnp.log(s).sum(), cov_diag_tree)))
+  norm_constant = -0.5 * (n_params * jnp.log(2 * math.pi) + cov_logdet)
+
+  def log_prior(params):
+    """Computes the Gaussian prior log-density."""
+    quadratic_term = sum(jax.tree_leaves(jax.tree_multimap(
+      lambda p, s: -(p**2 / (2 * s)).sum(), params, cov_diag_tree)))
+    log_prob = quadratic_term + norm_constant
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    """Computes the delta in  Gaussian prior log-density."""
+    diff = sum(jax.tree_leaves(jax.tree_multimap(
+        lambda p1, p2, s: -((p1**2 - p2**2) / (2 * s)).sum(),
+        params1, params2, cov_diag_tree)))
+    return diff / temperature
+
+  return log_prior, log_prior_diff
+
+
+def make_lenet_conv_correlated_log_prior(weight_decay, inv_sigma, temperature):
+  """Returns the block-correlated log-density and delta given weight decay."""
+
+  gaussian_prior, gaussian_diff = make_gaussian_log_prior(
+      weight_decay, temperature)
+  def log_prior(params):
+    """Computes the Gaussian prior log-density."""
+    n_params = sum([p.size for p in jax.tree_leaves(params)])
+    log_prob = - 0.5 * jnp.trace(jnp.matmul(
+      jnp.matmul(params["conv2_d"]["w"].reshape(25, 6).T, inv_sigma),
+      params["conv2_d"]["w"].reshape(25, 6)))
+    log_prob += gaussian_prior(params)
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    """Computes the delta in block-correlated prior log-density."""
+    return (log_prior(params1) - log_prior(params2) +
+            gaussian_diff(params1, params2))
+
+  return log_prior, log_prior_diff
+
+
+def make_lenet_sumfilter_log_prior(
+  weight_decay, filter_weight_decay, temperature
+):
+  gaussian_prior, gaussian_diff = make_gaussian_log_prior(
+    weight_decay, temperature)
+  def log_prior(params):
+    kw, kh, cin, cout = params["conv2_d"]["w"].shape
+    sum_filter = jnp.sum(jnp.abs(
+      jnp.sum(params["conv2_d"]["w"].reshape(kw * kh, cin, cout), 0)))
+    log_prob = -0.5 * sum_filter * weight_decay
+    log_prob += gaussian_prior(params)
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    return log_prior(params1) - log_prior(params2)
+
+  return log_prior, log_prior_diff
+
+
+def make_lenet_pca_log_prior(
+  weight_decay, pca_inv_cov, pca_wd, temperature
+):
+  gaussian_prior, gaussian_diff = make_gaussian_log_prior(
+    weight_decay, temperature)
+  def log_prior(params):
+    kw, kh, cin, cout = params["conv2_d"]["w"].shape
+    w = params["conv2_d"]["w"].reshape(kw * kh * cin, cout)
+    log_prob = - jnp.einsum("ic,ij,jc->", w, pca_inv_cov, w)
+    log_prob *= pca_wd / 2
+
+    log_prob += gaussian_prior(params)
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    return log_prior(params1) - log_prior(params2)
+
+  return log_prior, log_prior_diff
+
+
+def make_mlp_pca_log_prior(
+  weight_decay, pca_inv_cov, pca_wd, temperature
+):
+  gaussian_prior, gaussian_diff = make_gaussian_log_prior(
+    weight_decay, temperature)
+  def log_prior(params):
+    # n_in, n_out = params["linear"]["w"].shape
+    w = params["linear"]["w"]
+    log_prob = - jnp.einsum("ic,ij,jc->", w, pca_inv_cov, w)
+    log_prob *= pca_wd / 2
+
+    log_prob += gaussian_prior(params)
+    return log_prob / temperature
+
+  def log_prior_diff(params1, params2):
+    return log_prior(params1) - log_prior(params2)
+
+  return log_prior, log_prior_diff
